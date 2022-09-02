1456a65306de62756096843e7d4469dcfd8cddfd
jstnfayricana <102305676+jstnfayricana@users.noreply.github.com>
2022-09-02 15:10:21 +0800

    Add files via upload

---
+++ activity2/Add2Numbers.py
@@ -0,0 +1,4 @@
+num1= int(input("number1 :"))
+num2= int(input("number2 :"))
+sum= num1 + num2
+print(sum)
---
+++ activity2/TriangleArea.py
@@ -0,0 +1,7 @@
+#area of triangle
+print("enter base of triangle: ")
+b=int(input())
+print("enter height of triangle: ")
+h=int(input())
+area=(b+h)/2
+print("area= ", area)
